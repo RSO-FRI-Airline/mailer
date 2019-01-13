d075d18a47fb8d3394a0b71c60319f1026c4579c
Jaka Kordez <jaka.kordez@gmail.com>
2019-01-13 10:45:31 +0100

    Added deployment configurations

--- api/src/main/java/si/fri/rso/api/MailEndpoint.java
+++ api/src/main/java/si/fri/rso/api/MailEndpoint.java
@@ -1,22 +1,16 @@
 package si.fri.rso.api;
 
 import com.kumuluz.ee.common.config.EeConfig;
-import com.kumuluz.ee.configuration.sources.FileConfigurationSource;
 import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.Properties;
 
 @Path("forecast")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -27,6 +21,40 @@ public class MailEndpoint {
     @ApplicationScoped
     @Path("/send")
     public Response get(Mail mail){
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                        String password = ConfigurationUtil.getInstance().get("kumuluzee.env.gmail").get();
+                        return new PasswordAuthentication("friairlines", password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("friairlines@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(mail.to));
+            message.setSubject(mail.subject);
+            message.setText(mail.content);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
         return Response.ok(mail).build();
     }
 }
